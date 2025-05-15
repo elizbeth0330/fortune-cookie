e99cf76ac78d6b4ab2c0190be741e5710c6c252c
elizabeth <eliz.beth0330@gmail.com>
2025-05-15 16:21:31 -0700

    updating css

--- script.js
+++ script.js
@@ -7,16 +7,25 @@ const fortunes = [
 
 const openCookieImageSrc = 'fortune-cookie-open.jpeg';
 
+console.log("JavaScript file loaded.");
+
 cookieImage.addEventListener('click', () => {
+    console.log("image clicked");
     cookieImage.removeEventListener('click', arguments.callee);
     cookieImage.classList.add('spinning');
+    console.log("spinning added");
     setTimeout(() => {
+        console.log("timeout finished");
         cookieImage.src = openCookieImageSrc;
+        console.log("image source changed");
         cookieImage.classList.remove('spinning');
+        console.log("spinning removed");
 
         const randomIndex = Math.floor(Math.random() * fortunes.length);
         const randomFortune = fortunes[randomIndex];
         fortuneMessage.textContent = randomFortune;
+        console.log("fortune generated:", randomFortune);
         fortuneMessage.classList.remove('hidden');
+        console.log("hidden class removed");
     }, 1000);
 });
