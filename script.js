9212e2ab7f7b0b5c535eb7f1b84bde1e00cb37fd
elizabeth <eliz.beth0330@gmail.com>
2025-05-15 16:45:29 -0700

    updated javascript functions

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
 const cookieImage = document.getElementById('cookie-image');
 const fortuneMessage = document.getElementById('fortune-message');
 const fortunes = [
-    "You will find happiness in unexpected places.", 
-    "A journey of a thousand miles begins with a single step."
+    "You will find happiness in unexpected places.",
+    "A journey of a thousand miles begins with a single step.",
+    "The early bird gets the worm, but the second mouse gets the cheese.",
+    "Good things are coming your way.",
+    "A smile is your passport.",
+    "Believe you can and you're halfway there.",
+    "The best is yet to come.",
+    "Today is your lucky day!",
+    "Don't be afraid to take risks.",
+    "Your creativity will lead you to success."
+    "A pleasant surprise is in store for you.",
+    "Your hard work will soon pay off.",
+    "Take time to enjoy the simple things.",
+    "Friendship is a treasure.",
+    "Laughter is the best medicine.",
+    "New opportunities will arise.",
+    "You have a kind heart.",
+    "Embrace change with an open mind.",
+    "The answer you seek is within you."
 ];
 
-const openCookieImageSrc = 'fortune-cookie-open.jpeg';
+const openCookieImageSrc = "fortune-cookie-open.jpg";
 
 console.log("JavaScript file loaded.");
 
-cookieImage.addEventListener('click', () => {
+function handleClickCookie() {
+    cookieImage.removeEventListener('click', handleClickCookie);
     console.log("image clicked");
-    cookieImage.removeEventListener('click', arguments.callee);
     cookieImage.classList.add('spinning');
     console.log("spinning added");
     setTimeout(() => {
         console.log("timeout finished");
         cookieImage.src = openCookieImageSrc;
-        console.log("image source changed");
+        console.log("img source changed");
         cookieImage.classList.remove('spinning');
         console.log("spinning removed");
-
+        
         const randomIndex = Math.floor(Math.random() * fortunes.length);
         const randomFortune = fortunes[randomIndex];
         fortuneMessage.textContent = randomFortune;
@@ -28,4 +45,6 @@ cookieImage.addEventListener('click', () => {
         fortuneMessage.classList.remove('hidden');
         console.log("hidden class removed");
     }, 1000);
-});
+}
+
+cookieImage.addEventListener('click', handleClickCookie);
